9e69061ace4484c2fb59554e4d0f46e01e2b0076
13949836783@163.com <13949836783@163.com>
2017-06-20 21:18:14 +0800

    add wage and email to job

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620124602) do
+ActiveRecord::Schema.define(version: 20170620125539) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.integer  "wage_upper_bound"
+    t.integer  "wage_lower_bound"
+    t.string   "contact_email"
   end
 
   create_table "resumes", force: :cascade do |t|
